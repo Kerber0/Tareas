33859cab4df2a00580e495dbb4e9a06e0cf18799
Nico <nprimoy@gmail.com>
2024-11-22 21:18:07 +0100

    sdfs

--- Base de datos/MySql/hospital.sql
+++ Base de datos/MySql/hospital.sql
@@ -1,21 +1,23 @@
-CREATE DATABASE IF NOT EXISTS Hospital;
-USE Hospital;
-CREATE TABLE IF NOT EXISTS Edificio (
-Nombre char (20) PRIMARY KEY,
+CREATE DATABASE IF NOT EXISTS Hospital;  /* Creamos la BD */
+USE Hospital;       /* Cambiamos a la BD con la que vamos a trabajar */
+CREATE TABLE IF NOT EXISTS Edificio (      /* Creamos una tabla  [IF NOT EXISTS] OPCIONAL []*/
+Nombre char (20) PRIMARY KEY,  /* [PK SOLO SI ES EN UNICO ATRIBUTO] [NOT NULL / AUTO_INCREMENT / DEFAULT VALOR / CHECK] EN TODOS LOS ATRIBUTOS */
 Calle char (20) NOT NULL,
 Numero int ,
 Cod_postal char (5) NOT NULL,
 Localidad char (20) NOT NULL
 );
 
+/* [OPCIONES DE TABLAS ( DATA DIRECTORY / INDEX DIRECTORY / ENGINE / AUTO_INCREMENT / COMMENT)]*/
+
 CREATE TABLE IF NOT EXISTS Planta (
 Nombre_edificio char (20),
 Nombre_planta char (20) NOT NULL,
 Numero_planta int NOT NULL,
-PRIMARY KEY (Nombre_edificio, Numero_planta),
-FOREIGN KEY (Nombre_edificio) REFERENCES Edificio (Nombre)
-ON DELETE CASCADE,
-ON UPDATE CASCADE
+PRIMARY KEY (Nombre_edificio, Numero_planta), /* En caso de que la PK sea compuesta se pone como restriccion abajo*/
+FOREIGN KEY (Nombre_edificio) REFERENCES Edificio (Nombre) /* Las FK tambien se ponen como restricciones abajo diciendo de donde viene*/
+ON DELETE CASCADE, /* "Caso borrar" Acciones a hacer en caso de modificiar la FK [ CASCADE | NO ACTION | SET NULL | SET DEFALUT | RESTRICT] */
+ON UPDATE CASCADE  /* "Caso actalizar" NO ACTION y RESTRICT  no se suele poner por que si no se pone nada queda por defecto]*/
 );
 
 CREATE TABLE IF NOT EXISTS Cama (
