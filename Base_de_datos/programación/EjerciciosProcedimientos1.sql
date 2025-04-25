6a90e8f0e974a9ed65d06b7d7da54c481a39fe0d
Nico <nprimoy@gmail.com>
2025-04-25 16:52:36 +0200

    123

--- Base_de_datos/programación/EjerciciosProcedimientos1.sql
+++ Base_de_datos/programación/EjerciciosProcedimientos1.sql
@@ -30,4 +30,76 @@ end if;
 end//
 
 /*7*/
-call PostoEmpregado4(null)//
+create FUNCTION divisible(a int, b int)
+RETURNs int 
+deterministic
+begin
+declare resto DOUBLE;
+set resto = mod(a,b);
+if resto = 0 then
+RETURN 1;
+else RETURN 0;
+end if;
+return mayor;
+end//
+
+
+/*8*/
+
+DELIMITER //
+
+CREATE FUNCTION mayorDeTres(a INT, b INT, c INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE mayor INT;
+
+    SET mayor = a;
+
+    IF mayor < b THEN
+        SET mayor = b;
+    ELSEIF mayor < c THEN
+        SET mayor = c;
+    END IF;
+
+    RETURN mayor;
+END;
+
+
+
+/*9*/
+
+CREATE FUNCTION ganador(resultado CHAR(7))
+RETURNS CHAR(1)
+DETERMINISTIC
+BEGIN
+    DECLARE hogar INT;
+    DECLARE visitante INT;
+
+    SET hogar = CAST(SUBSTRING_INDEX(resultado, '-', 1) AS int);
+    SET visitante = CAST(SUBSTRING_INDEX(resultado, '-', -1) AS int);
+
+    IF hogar > visitante THEN
+        RETURN '1';
+    ELSE
+        RETURN '0';
+    END IF;
+END //
+
+
+/*10*/
+
+create procedure palindromo(palabra char(10))
+begin
+if palabra = reverse(palabra) then
+select "si" as "Palíndromo";
+else select  "no" as "Palíndromo";
+end if;
+end// 
+
+
+/*11*/
+
+/*12*/
+
+/*13*/
